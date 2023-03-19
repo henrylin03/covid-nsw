91ea6f22235f9e3db102ec3148677b469de2a0c9
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-03-19 11:42:01 +1100

    find start date, set configurations, create date range filter (no linkage to data yet) in sidebar

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import sys
+import datetime
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -7,18 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
 
-def run_dashboard():
-    st.title("COVID in NSW")
-    st.write(f"_Last updated: **{get_last_updated_date()}**_")
-
-    # test lgas
-    lga_name = st.sidebar.selectbox(
-        "Select Local Government Area (LGA)",
-        get_lgas(),
-    )
-
-
-def setup_chromedriver():
+def setup_chromedriver() -> webdriver:
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_argument("--start-maximized")
@@ -38,19 +28,27 @@ def load_and_clean_csv() -> pd.DataFrame:
         .str.replace(" (C)", "", regex=False)
         .str.replace(" (NSW)", "", regex=False)
     )
+    # df["date"] = pd.to_datetime(df.date, format="%Y-%m-%d")
     return df
 
 
-def get_last_updated_date() -> str:
+def get_start_date() -> datetime.date:
+    covid_df = load_and_clean_csv()
+    first_case_date = min(covid_df.date)
+    first_case_date_datetime = datetime.datetime.strptime(first_case_date, "%Y-%m-%d")
+    return first_case_date_datetime.date()
+
+
+def get_last_updated_date():
     DRIVER = setup_chromedriver()
     data_nsw_url = "https://data.nsw.gov.au/search/dataset/ds-nsw-ckan-aefcde60-3b0c-4bc0-9af1-6fe652944ec2/details?q="
     DRIVER.get(data_nsw_url)
-    last_updated_date = DRIVER.find_element(
+    last_updated_date_str = DRIVER.find_element(
         By.XPATH, "//span[@itemprop='dateModified']"
     ).text
     DRIVER.close()
 
-    return datetime.strptime(last_updated_date, "%d/%m/%Y").strftime("%d %b %Y")
+    return datetime.datetime.strptime(last_updated_date_str, "%d/%m/%Y").date()
 
 
 def get_lgas() -> tuple:
@@ -61,5 +59,26 @@ def get_lgas() -> tuple:
     return tuple(sorted(lgas_filtered))
 
 
-if __name__ == "__main__":
-    run_dashboard()
+covid_df = load_and_clean_csv()
+dataset_start_date = get_start_date()
+dataset_last_updated_date = get_last_updated_date()
+dataset_last_updated_date_formatted = dataset_last_updated_date.strftime("%d %b %Y")
+
+st.set_page_config(
+    page_title="COVID in NSW", page_icon=":adhesive_bandage:", layout="wide"
+)
+st.title(":adhesive_bandage: COVID in NSW")
+st.write(f"_last updated: **{dataset_last_updated_date_formatted}**_")
+st.dataframe(covid_df, use_container_width=True)
+
+st.sidebar.header("Filters")
+lga_name = st.sidebar.selectbox(
+    "Local Government Area (LGA)",
+    get_lgas(),
+)
+date_range = st.sidebar.date_input(
+    "Date Range",
+    min_value=dataset_start_date,
+    max_value=dataset_last_updated_date,
+    value=[dataset_start_date, dataset_last_updated_date],
+)
