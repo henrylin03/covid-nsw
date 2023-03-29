c58d8b7f41e51b1266bf747fba954b746f2c1bbb
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-03-30 08:52:26 +1100

    improve reported cases area plot incl resolution and dpi

--- dashboard.py
+++ dashboard.py
@@ -69,15 +69,16 @@ def get_lgas() -> tuple:
     return tuple(lgas_filtered_sorted)
 
 
-def plot_area_chart(input_df: pd.DataFrame):
+def plot_daily_cases_area_chart(input_df: pd.DataFrame):
     daily_cases = input_df.groupby("date").sum().reset_index()
     daily_cases.date = pd.to_datetime(daily_cases.date, format="%Y-%m-%d")
 
     sns.set_style("dark", {"axes.facecolor": "0.95"})
     sns.set_palette("dark")
 
-    fig, ax = plt.subplots(figsize=(10, 2))
-    sns.lineplot(x="date", y="cases_count", data=daily_cases, ax=ax, linewidth=1.1)
+    fig, ax = plt.subplots(figsize=(7, 2), dpi=1000)
+    # fig.set_dpi(500)
+    sns.lineplot(x="date", y="cases_count", data=daily_cases, ax=ax, linewidth=0.8)
     plt.fill_between(x=daily_cases.date, y1=daily_cases.cases_count, alpha=0.2)
 
     ax.xaxis.set_minor_locator(md.MonthLocator(bymonth=range(13)))
@@ -154,7 +155,7 @@ def main():
 
     # visualisations
     st.markdown("**Daily Cases**")
-    daily_cases_area_chart = plot_area_chart(covid_df)
+    daily_cases_area_chart = plot_daily_cases_area_chart(covid_df)
     st.pyplot(daily_cases_area_chart)
     # st.area_chart(data=total_daily_cases, x="date", y="cases_count")
 
