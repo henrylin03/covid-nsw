828667e2ce7ecd0178128e14da2f935e34e85892
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-04-12 08:18:22 +1000

    make constants all capitals (urls)

--- dashboard.py
+++ dashboard.py
@@ -14,8 +14,8 @@ import seaborn as sns
 
 @st.cache_data
 def load_and_clean_csv() -> pd.DataFrame:
-    csv_url = "https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/5d63b527-e2b8-4c42-ad6f-677f14433520/download/confirmed_cases_table1_location_agg.csv"
-    df = pd.read_csv(csv_url)
+    CSV_URL = "https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/5d63b527-e2b8-4c42-ad6f-677f14433520/download/confirmed_cases_table1_location_agg.csv"
+    df = pd.read_csv(CSV_URL)
     df = df[["notification_date", "lga_name19", "confirmed_cases_count"]]
     df.columns = ["date", "lga", "cases_count"]
     df = df.groupby(["date", "lga"]).cases_count.sum().reset_index()
@@ -39,8 +39,8 @@ def get_start_date() -> datetime.date:
 @st.cache_data
 def get_last_updated_date() -> datetime.date:
     DRIVER = setup_chromedriver()
-    data_nsw_url = "https://data.nsw.gov.au/search/dataset/ds-nsw-ckan-aefcde60-3b0c-4bc0-9af1-6fe652944ec2/details?q="
-    DRIVER.get(data_nsw_url)
+    DATA_NSW_URL = "https://data.nsw.gov.au/search/dataset/ds-nsw-ckan-aefcde60-3b0c-4bc0-9af1-6fe652944ec2/details?q="
+    DRIVER.get(DATA_NSW_URL)
     last_updated_date_str = DRIVER.find_element(
         By.XPATH, "//span[@itemprop='dateModified']"
     ).text
