b7cf2b3d4c1eb2689e798d310772ff94ea047c1c
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-07-24 11:20:44 +1000

    attempt to fix deployment issues by updating requirements.txt

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import datetime
 import streamlit as st
 import wikipedia
-from selenium import webdriver
+from selenium:4.10.0 import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
